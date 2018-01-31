e01eee107d094559eef94cfec9ed506d86f6b7a5
Kulkarni <kavita.kulkarni@capgemini.com>
2018-01-31 18:56:15 +0530

    Done

---
+++ TestPracticeI/src/com/test/WebCalenderDemo/WebCalender.java
@@ -0,0 +1,21 @@
+package com.test.WebCalenderDemo;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebCalender {
+
+	public static WebDriver driver;
+	public static String chDriverPath = "D:/Kavita/Drivers/";
+
+	public static void BasicProces() {
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\Kavita\\Drivers\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://redbus.in");
+	}
+	
+	public static void main(String[] args) {
+		BasicProces();
+	}
+}
